b8af2a5b2251046b48438e9080d284f5e27a0fad
Chhunneng <chreachanchhunneng18@kit.edu.kh>
2019-02-28 15:45:45 +0700

    add day

--- Day of Birthday.c
+++ Day of Birthday.c
@@ -1,5 +1,50 @@
 #include<stdio.h>
+struct month{
+	int date[12];
+	char day[7];
+}mon;
 main()
 {
-	int day,month,year;
+	int year,i;
+	printf("Enter year: ");
+	scanf("%d",&year);
+	mon.date[0]=31;
+	mon.date[1]=28;
+	mon.date[2]=31;
+	mon.date[3]=30;
+	mon.date[4]=31;
+	mon.date[5]=30;
+	mon.date[6]=31;
+	mon.date[7]=31;
+	mon.date[8]=30;
+	mon.date[9]=31;
+	mon.date[10]=30;
+	mon.date[11]=31;
+	if(year%400==0 && year%100==0)
+	mon.date[1]=29;
+	else if(year%4==0)
+	mon.date[1]=29;
+	else
+	mon.date[1]=mon.date[1];
+	
+	
+	
+	for(i=0;i<12;i++)
+	{
+		printf("This year Month %d have %d days\n",i+1,mon.date[i]);
+	}
+	
+	mon.day[0]="Sunday";
+	mon.day[1]="Monday";
+	mon.day[2]="Tuesday";
+	mon.day[3]="Wednesday";
+	mon.day[4]="Thursday";
+	mon.day[5]="Friday";
+	mon.day[6]="Saturday";
+	
+	for(i=0;i<7;i++)
+	{
+		printf("Day %d is %c\n",i+1,mon.day[i]);
+	}
+	
 }
